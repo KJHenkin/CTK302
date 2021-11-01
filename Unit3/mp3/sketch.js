1d46e10fd1cea6d3e5433289419346cbf3bef1d8
KJHenkin <kjhenk1@ilstu.edu>
2021-11-01 12:11:44 -0500

    yeet

---
+++ Unit3/mp3/sketch.js
@@ -0,0 +1,145 @@
+let cars = [];
+let frogPos;
+let maxCar = 1;
+let timer = 0;
+let state = 0;
+let camera ;
+let film ;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER) ;
+
+camera = loadImage("assets/camera.svg") ;
+film = loadImage("assets/film.svg") ;
+
+  // Spawn many objects
+
+  for (let i = 0; i < maxCar; i++) {
+    cars.push(new Car());
+  }
+
+  frogPos = createVector(width / 2, height - 80);
+  imageMode(CENTER) ;
+  textAlign(CENTER) ;
+  rectMode(CENTER) ;
+  ellipseMode(CENTER) ; 
+
+}
+
+function draw() {
+  switch (state) {
+    case 0: //welcome
+      background("beige");
+      textSize(28);
+      fill("black");
+      text("Welcome - Collect the film to finish the photoshoot", 100, 200);
+      break;
+
+    case 1: //game state
+      game();
+      timer++;
+      if (timer >  10* 60) {
+        timer = 0;
+        state = 3;
+      }
+      break;
+
+    case 2: //win state
+      background("lightgreen");
+      textSize(28);
+      fill("black");
+      text("YOU DID IT! YOU COMPLETED THE SHOOT!", 100, 100);
+      break;
+
+    case 3: //lose state
+      textSize(28);
+      fill("white");
+      background("lightred");
+      text("YOU RAN OUT OF FILM. YOU FAILED TO FINISH THE SHOOT", 100, 100);
+      break;
+  }
+}
+
+function mouseReleased() {
+  switch (state) {
+    case 0:
+      state = 1;
+      break;
+
+    case 2: //they won
+      resetTheGame();
+      state = 0;
+      break;
+
+    case 3: //they lost
+      resetTheGame();
+      state = 0;
+      break;
+  }
+}
+
+function game() {
+  background("grey");
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+
+    if (cars[i].pos.dist(frogPos) < 50) {
+      cars.splice(i, 1);
+    }
+  }
+
+  if (cars.length == 0) {
+    state = 2;
+  }
+
+  // frog
+  fill("green");
+  image(camera, frogPos.x, frogPos.y, 50, 50);
+  checkForKeys();
+}
+
+function resetTheGame() {
+  timer = 0;
+  cars = [];
+
+  //spawn many objects
+  for (let i = 0; i < maxCar; i++) {
+    cars.push(new Car());
+  }
+}
+
+function checkForKeys() {
+  if (keyIsDown(LEFT_ARROW)) frogPos.x -= 5;
+  if (keyIsDown(RIGHT_ARROW)) frogPos.x += 5;
+  if (keyIsDown(UP_ARROW)) frogPos.y -= 5;
+  if (keyIsDown(DOWN_ARROW)) frogPos.y += 5;
+}
+
+// Car class
+class Car {
+  // constructor and attributes
+  constructor() {
+    this.pos = createVector(200, 200);
+    this.vel = createVector(random(-5, 5), random(-5, 5));
+    this.col = color(0, random(50, 100), random(100, 255), random(255));
+    this.size = random(50, 80);
+  }
+
+  // methods
+
+  display() {
+    fill(this.col);
+    textSize(this.size);
+    image(film, this.pos.x, this.pos.y);
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
+}
---
+++ Unit3/particlesystem/sketch.js
@@ -0,0 +1,58 @@
+let cars = [];
+let paint ;
+
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+
+  // for (let i = 0; i < 20; i++) {
+  //   cars.push(new Car());
+  // }
+}
+
+function draw() {
+  background("#008080");
+
+  image(paint, 100, 100) ; 
+
+  cars.push(new Car()); //spawn car
+
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+
+    if (cars[i].a <= 0) {
+      cars.splice(i, 1);
+    }
+  }
+
+  print(cars.length);
+}
+
+class Car {
+  constructor() {
+    // attributes
+    this.pos = createVector(100, 100);
+    this.vel = createVector(random(5, 10), random(5, 10));
+    this.a = random(200, 255);
+    this.r = random(255) ;
+    this.g = random(255) ;
+    this.b = random(255) ;
+    this.size = random(10, 25) ;
+  }
+
+  // methods
+  display() {
+    fill(this.r, this.g, this.b, this.a);
+    text("hello",this.pos.x, this.pos.y, this.size, this.size);
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    this.a -= 3;
+    // if (this.pos.x > width) this.pos.x = 0;
+    // if (this.pos.x < 0) this.pos.x = width;
+    // if (this.pos.y > height) this.pos.y = 0;
+    // if (this.pos.y < 0) this.pos.y = height;
+  }
+}
